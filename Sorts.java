bf7a572d0cdf6a54bca6e1e061f3727ec834960f
Allen Shen <ashen1@stuy.edu>
2018-12-05 05:37:36 -0500

    compile errors debugged

--- Sorts.java
+++ Sorts.java
@@ -1,12 +1,15 @@
 public class Sorts {
   public static void selectionSort(int [] ary){
-    for (int i = 0; i < ary.length; i++)
-      int store = ary[i];
-      for (int j = 0; j < ary.length - i; j++){
+    int store;
+    for (int i = 0; i < ary.length; i++){
+      store = ary[i];
+      for (int j = i; j < ary.length; j++){
         if (ary[j] < store){store = ary[j];}
       }
     }
+  }
   public static void main(String[] args){
-
+      int[] list = new int[]{42, 11, 32, 99};
+      System.out.println(list);
     }
   }
