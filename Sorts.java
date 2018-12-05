234e7f836b4dd32eae125d6d3694ef84dc05e116
Allen Shen <ashen1@stuy.edu>
2018-12-05 11:16:39 -0500

    second branch finished version

--- Sorts.java
+++ Sorts.java
@@ -1,13 +1,25 @@
 public class Sorts {
   public static void selectionSort(int [] ary){
-    int store;
+    int store, insert;
     for (int i = 0; i < ary.length; i++){
       store = ary[i];
+      insert = ary[i];
       for (int j = i; j < ary.length; j++){
-        if (ary[j] < store){store = ary[j];}
+        if (ary[j] < store){
+          store = ary[j];
+          ary[j] = insert;
+          insert = store;
+        }
       }
       ary[i] = store;
     }
+  }
+  public static int[] toIntArray(String[] args){
+    int[] result = new int[args.length];
+    for (int i = 0; i < args.length; i++){
+      result[i] = Integer.parseInt(args[i]);
+    }
+    return result;
   }
   public static void main(String[] args){
       int[] list = new int[]{42, 11, 32, 99};
