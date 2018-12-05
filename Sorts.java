48fa6d21666dd57fda012d89c2bc7d3145b3cdcf
Allen Shen <ashen1@stuy.edu>
2018-12-05 05:29:45 -0500

    first draft done

---
+++ Sorts.java
@@ -0,0 +1,12 @@
+public class Sorts {
+  public static void selectionSort(int [] ary){
+    for (int i = 0; i < ary.length; i++)
+      int store = ary[i];
+      for (int j = 0; j < ary.length - i; j++){
+        if (ary[j] < store){store = ary[j];}
+      }
+    }
+  public static void main(String[] args){
+
+    }
+  }
